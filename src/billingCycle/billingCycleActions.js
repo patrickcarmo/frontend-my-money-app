ede82cb43ebdcbcd945a616f87ae16c361a0700d
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-02 13:28:18 +0100

    Mensagem de Sucesso e Erro - Toast

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toastr } from 'react-redux-toastr';
 
 const BASE_URL = 'http://localhost:3003/api';
 
@@ -12,7 +13,13 @@ export function getList() {
 
 export function create(values) {
     console.log(values);
-    axios.post(`${BASE_URL}/billingCycles`, values);
+    axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.');
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error));
+        })
     return {
         type: 'TEMP'
     }

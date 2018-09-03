ec6b1adc0427d9d822b777a2381950b1bb7cdb99
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-03 20:41:11 +0100

    Alterar Ciclo de Pagamento - PUT

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -39,7 +39,7 @@ class BillingCycleForm extends Component {
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
-                    <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button>
+                    <button type='button' className='btn btn-default pull-right' onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )

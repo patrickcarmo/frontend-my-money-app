cd011c7032d998f500c09abe7e4f389f9bb91e6b
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-06 13:18:18 +0100

    Final

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -1,6 +1,6 @@
 const userKey = '_mymoney_user';
 const INITIAL_STATE = {
-    user: {name: 'Teste', email: 'teste@test.com'}, //JSON.parse(localStorage.getItem(userKey)),
+    user: JSON.parse(localStorage.getItem(userKey)),
     validToken: false
 };
 
